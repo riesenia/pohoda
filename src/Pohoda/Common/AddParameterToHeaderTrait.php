9668a47eff64516df1eb78134902adc363e156af
Tomas Saghy <segy@riesenia.com>
2018-05-17 06:46:56 +0200

    fixer

--- src/Pohoda/Common/AddParameterToHeaderTrait.php
+++ src/Pohoda/Common/AddParameterToHeaderTrait.php
@@ -15,9 +15,9 @@ trait AddParameterToHeaderTrait
     /**
      * Set user-defined parameter.
      *
-     * @param string      $name  (can be set without preceding VPr / RefVPr)
-     * @param string      $type
-     * @param mixed       $value
+     * @param string     $name  (can be set without preceding VPr / RefVPr)
+     * @param string     $type
+     * @param mixed      $value
      * @param mixed|null $list
      *
      * @return \Riesenia\Pohoda\Agenda
--- src/Pohoda/Common/AddParameterTrait.php
+++ src/Pohoda/Common/AddParameterTrait.php
@@ -17,9 +17,9 @@ trait AddParameterTrait
     /**
      * Set user-defined parameter.
      *
-     * @param string      $name  (can be set without preceding VPr / RefVPr)
-     * @param string      $type
-     * @param mixed       $value
+     * @param string     $name  (can be set without preceding VPr / RefVPr)
+     * @param string     $type
+     * @param mixed      $value
      * @param mixed|null $list
      *
      * @return \Riesenia\Pohoda\Agenda
