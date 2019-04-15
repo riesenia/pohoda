58cd17e95d79ceaccaf4523322bfd3a725afb575
Tomas Saghy <segy@riesenia.com>
2019-04-15 10:40:03 +0200

    addressbook p1 - p6

--- src/Pohoda/Addressbook/Header.php
+++ src/Pohoda/Addressbook/Header.php
@@ -23,7 +23,7 @@ class Header extends Agenda
     protected $_refElements = ['centre', 'activity', 'contract', 'number'];
 
     /** @var array */
-    protected $_elements = ['identity', 'region', 'phone', 'mobil', 'fax', 'email', 'web', 'ICQ', 'Skype', 'GPS', 'credit', 'priceIDS', 'maturity', 'paymentType', 'agreement', 'number', 'ost1', 'ost2', 'message', 'note', 'intNote', 'centre', 'activity', 'contract'];
+    protected $_elements = ['identity', 'region', 'phone', 'mobil', 'fax', 'email', 'web', 'ICQ', 'Skype', 'GPS', 'credit', 'priceIDS', 'maturity', 'paymentType', 'agreement', 'number', 'ost1', 'ost2', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'message', 'note', 'intNote', 'centre', 'activity', 'contract'];
 
     /**
      * {@inheritdoc}
@@ -75,6 +75,12 @@ class Header extends Agenda
         $resolver->setNormalizer('agreement', $resolver->getNormalizer('string12'));
         $resolver->setNormalizer('ost1', $resolver->getNormalizer('string8'));
         $resolver->setNormalizer('ost2', $resolver->getNormalizer('string8'));
+        $resolver->setNormalizer('p1', $resolver->getNormalizer('bool'));
+        $resolver->setNormalizer('p2', $resolver->getNormalizer('bool'));
+        $resolver->setNormalizer('p3', $resolver->getNormalizer('bool'));
+        $resolver->setNormalizer('p4', $resolver->getNormalizer('bool'));
+        $resolver->setNormalizer('p5', $resolver->getNormalizer('bool'));
+        $resolver->setNormalizer('p6', $resolver->getNormalizer('bool'));
         $resolver->setNormalizer('message', $resolver->getNormalizer('string64'));
     }
 }
