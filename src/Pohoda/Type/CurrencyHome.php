6c48f4f03acb1091da96a9fb486bf31d7d9a2361
Ciki <matula.m@gmail.com>
2019-09-27 10:47:38 +0200

    CurrencyHome: add round element

--- src/Pohoda/Type/CurrencyHome.php
+++ src/Pohoda/Type/CurrencyHome.php
@@ -21,7 +21,7 @@ class CurrencyHome extends Agenda
     use SetNodeNameTrait;
 
     /** @var array */
-    protected $_elements = ['priceNone', 'price3', 'price3VAT', 'price3Sum', 'priceLow', 'priceLowVAT', 'priceLowSum', 'priceHigh', 'priceHighVAT', 'priceHighSum'];
+    protected $_elements = ['priceNone', 'price3', 'price3VAT', 'price3Sum', 'priceLow', 'priceLowVAT', 'priceLowSum', 'priceHigh', 'priceHighVAT', 'priceHighSum', 'round'];
 
     /**
      * {@inheritdoc}
