094ca7578dcf9316d0dddbe4a2853edc15779436
Daniel Rosa <rosskodca@gmail.com>
2026-05-14 13:48:53 +0200

    Add support for `correctionMOSS` (#62)
    * Add support for `correctionMOSS` in invoice header
    
    * Fix formatting issue in InvoiceSpec test case
    
    ---------
    
    Co-authored-by: Daniel Rosa <drosa@factcool.com>

--- src/Pohoda/Invoice/Header.php
+++ src/Pohoda/Invoice/Header.php
@@ -18,7 +18,7 @@ class Header extends DocumentHeader
     protected $_refElements = ['extId', 'number', 'accounting', 'classificationVAT', 'classificationKVDPH', 'order', 'paymentType', 'priceLevel', 'account', 'paymentAccount', 'centre', 'activity', 'contract', 'regVATinEU', 'MOSS', 'evidentiaryResourcesMOSS', 'carrier'];
 
     /** @var string[] */
-    protected $_elements = ['extId', 'invoiceType', 'number', 'symVar', 'originalDocument', 'originalDocumentNumber', 'symPar', 'date', 'dateTax', 'dateAccounting', 'dateKHDPH', 'dateDue', 'dateApplicationVAT', 'dateDelivery', 'accounting', 'classificationVAT', 'classificationKVDPH', 'numberKHDPH', 'text', 'partnerIdentity', 'myIdentity', 'order', 'numberOrder', 'dateOrder', 'paymentType', 'priceLevel', 'account', 'symConst', 'symSpec', 'paymentAccount', 'paymentTerminal', 'centre', 'activity', 'contract', 'regVATinEU', 'MOSS', 'evidentiaryResourcesMOSS', 'accountingPeriodMOSS', 'dateTaxOriginalDocumentMOSS', 'note', 'carrier', 'intNote'];
+    protected $_elements = ['extId', 'invoiceType', 'number', 'symVar', 'originalDocument', 'originalDocumentNumber', 'symPar', 'date', 'dateTax', 'dateAccounting', 'dateKHDPH', 'dateDue', 'dateApplicationVAT', 'dateDelivery', 'accounting', 'classificationVAT', 'classificationKVDPH', 'numberKHDPH', 'text', 'partnerIdentity', 'myIdentity', 'order', 'numberOrder', 'dateOrder', 'paymentType', 'priceLevel', 'account', 'symConst', 'symSpec', 'paymentAccount', 'paymentTerminal', 'centre', 'activity', 'contract', 'regVATinEU', 'MOSS', 'evidentiaryResourcesMOSS', 'accountingPeriodMOSS', 'dateTaxOriginalDocumentMOSS', 'correctionMOSS', 'carrier', 'note', 'intNote'];
 
     protected function _configureOptions(OptionsResolver $resolver)
     {
@@ -45,5 +45,6 @@ class Header extends DocumentHeader
         $resolver->setNormalizer('symSpec', $resolver->getNormalizer('string16'));
         $resolver->setNormalizer('paymentTerminal', $resolver->getNormalizer('bool'));
         $resolver->setNormalizer('dateTaxOriginalDocumentMOSS', $resolver->getNormalizer('date'));
+        $resolver->setNormalizer('correctionMOSS', $resolver->getNormalizer('bool'));
     }
 }
